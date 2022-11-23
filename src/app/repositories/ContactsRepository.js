0d04b126332d2e030a2dbd124a7a15cd46c5f9d8
alissonvieira-linux <alissonvieira.github@gmail.com>
2022-11-23 17:07:30 -0300

    feat: substituindo uuidv4 por uuid

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,12 +1,12 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
 const contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'Alisson Vieira',
     email: 'alisson@email.com',
     phone: '12987654321',
-    category_id: uuid(),
+    category_id: v4(),
   },
 ];
 
