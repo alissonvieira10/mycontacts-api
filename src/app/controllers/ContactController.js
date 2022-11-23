6725a287211940e3947c5e4e9e32691b1fa34e48
alissonvieira-linux <alissonvieira.github@gmail.com>
2022-11-23 17:26:32 -0300

    feat: implementando rota para deletar um contato

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -27,8 +27,18 @@ class ContactController {
 
   }
 
-  delete() {
+  async delete(request, response) {
+    const { id } = request.params;
 
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    await ContactsRepository.delete(id);
+
+    response.sendStatus(204);
   }
 }
 
--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,10 +1,17 @@
 const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
     id: v4(),
     name: 'Alisson Vieira',
     email: 'alisson@email.com',
+    phone: '12987654321',
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Matheus Silva',
+    email: 'mateus@email.com',
     phone: '12987654321',
     category_id: v4(),
   },
@@ -20,6 +27,13 @@ class ContactsRepository {
   findById(id) {
     return new Promise((resolve) => {
       resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
